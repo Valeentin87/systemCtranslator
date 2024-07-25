35eebc3b2c1450a2dd5621c5fe04b335eb4d0847
Valeentin87 <valeentin87@list.ru>
2024-07-25 11:37:43 +0300

    Заполнил данные по всем физическим величинам

--- index.js
+++ index.js
@@ -4,21 +4,27 @@ import { data }  from "./dataFile.js";
 
 const magnitude = data
 
+
+/**
+ * * provides information about the value specified in the parameter according to the C measurement system
+*  @param {string} magnitude - name of physical quantity
+*/
 function getInfoOfmagnitude(magnitudeName) {
+
     let result = null;
     let flag = false;
 
     for (let item = 0; item < magnitude.length; item++) {
         const element = magnitude[item];
         //console.log(item);
-        if (magnitude[item].name === magnitudeName) {
+        if (magnitude[item].magnitude === magnitudeName) {
             
             result = `
-                наименование         ----- ${magnitude[item].name}
-                наименование по ОКЕИ ----- ${magnitude[item].nameOKEI}
-                сокращение по ОКЕИ   ----- ${magnitude[item].shortNameOKEI}
-                код ОКЕИ             ----- ${magnitude[item].OKEI}
-                код КСПОВО           ----- ${magnitude[item].KSPOVO}
+                величина                                     ----- ${magnitude[item].magnitude}
+                Единица измерения (русское название)         ----- ${magnitude[item]['unit(rus)']}
+                Единица измерения (международное название)   ----- ${magnitude[item]['unit(intern)']}
+                Обозначение(русское)                         ----- ${magnitude[item]['designation(rus)']}
+                Обозначение(международное)                   ----- ${magnitude[item]['designation(intern)']}
                 `;
             flag = true;
         }
@@ -28,11 +34,28 @@ function getInfoOfmagnitude(magnitudeName) {
         };
     
     };
-    return `по физической величине ${magnitudeName} результат не найден...`;    
+    return `
+    по физической величине ${magnitudeName} результат не найден...
+    для получения справки по названиям всех физических величин воспользуйтесь функцией getAllNamesoFMagnitudes
+    `;    
     };
 
+/**
+ * provides information about the names of all physical quantities
+ */
+function  getAllNamesoFMagnitudes() {
+    const allNames = [];
+    data.forEach(element => {
+        allNames.push(element.magnitude)
+    });
 
-    
+
+    console.log('Названия всех физических величин, о которых можно получить информацию: ');
+    allNames.forEach(name => {
+        console.log(name);
         
+    });
 
-console.log(getInfoOfmagnitude('KGM'));
+}   
+    
+module.exports = { getAllNamesoFMagnitudes, getInfoOfmagnitude };
